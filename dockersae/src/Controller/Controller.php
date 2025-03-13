656cb96393f2b34bb164faad7683e0390766954b
MaelValin <maelmy19@gmail.com>
2025-03-13 15:37:27 +0100

    symphonie mis en place

--- dockersae/src/Controller/Controller.php
+++ dockersae/src/Controller/Controller.php
@@ -9,22 +9,31 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 /* le nom de la classe doit être cohérent avec le nom du fichier */
-class TestController
+class Controller
 {
     // L'attribute #[Route] indique ici que l'on associe la route
     // "/" à la méthode home() pour que Symfony l'exécute chaque fois
     // que l'on accède à la racine de notre site.
 
-    #[Route('/home', name: 'home')]
+    #[Route('/', name: 'home')]
     public function home()
     {
-        die("Mael Valin");
+        die("home");
+        
+    }
+
+    #[Route('/test', name: 'test')]
+    public function test()
+    {
+        return new Response('<html><body><h1>Test Page</h1><p>This is a test page.</p></body></html>');
         
     }
 
     #[Route('/me', name: 'home')]
     public function me()
     {
-        die("Mael Valin");
+        $response = new Response("hello");
+        return $response;
+        
     }
 }
